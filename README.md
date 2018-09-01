a4d0e044370e7e2788ac146a31f4333456a194a2
Allen Garvey <allen.garvey@gmail.com>
2018-08-31 21:30:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Linux Huion Config
+
+Config files and instructions for using a Huion H1060P drawing tablet with Linux
+
+## Dependencies
+
+* Ubuntu >= 18.04
+* Huion H1060P drawing tablet
+* [digimend-kernel-drivers](https://github.com/DIGImend/digimend-kernel-drivers)
+
+## Getting Started
+
+* Install digiment-kernal-drivers
+* Make sure to follow directions to add
+`Section "InputClass"
+    Identifier "Tablet"
+    Driver "wacom"
+    MatchDevicePath "/dev/input/event*"
+    MatchUSBID "256c:006e"
+EndSection`
+
+to `/usr/share/X11/xorg.conf.d/70-wacom.conf`)
+* Restart computer
+* Run `./xsetwacom-krita-config.sh` to set buttons to Krita shortcuts
+
+## License
+
+Linux Huion Config is released under the MIT License. See license.txt for more details.
---
+++ xsetwacom-krita-config.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+# Huion H1060P xsetwacom configuration for Krita shortcuts
+
+#help for xsetwacom: https://www.mankier.com/1/xsetwacom
+
+#button order is 1-3, 8-16 from top left to bottom right
+
+xsetwacom set "Tablet Monitor Pad pad" button 1 key Ctrl Z
+xsetwacom set "Tablet Monitor Pad pad" button 2 key Ctrl Shift Z
+xsetwacom set "Tablet Monitor Pad pad" button 3 key Ctrl Shift O #custom lasso tool shortcut
+xsetwacom set "Tablet Monitor Pad pad" button 8 key Ctrl Shift A
+xsetwacom set "Tablet Monitor Pad pad" button 9 key P
+xsetwacom set "Tablet Monitor Pad pad" button 10 key E
+xsetwacom set "Tablet Monitor Pad pad" button 11 key B
+xsetwacom set "Tablet Monitor Pad pad" button 12 key +space
+xsetwacom set "Tablet Monitor Pad pad" button 13 key Ctrl Shift +plus
+xsetwacom set "Tablet Monitor Pad pad" button 14 key Ctrl +minus
+xsetwacom set "Tablet Monitor Pad pad" button 15 key +bracketleft
+xsetwacom set "Tablet Monitor Pad pad" button 16 key +bracketright
